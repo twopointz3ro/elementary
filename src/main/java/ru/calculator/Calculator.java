8a4b5b524b71c3431dbc813f240ecf252bc8b740
twopointz3ro <twopointzero404@gmail.com>
2024-12-02 18:41:28 +0100

    1.3 Аримфметические операции

--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
         int six = 6;
         int four = 4;
         int five = 5;
-        int sixDivTwo = six /two;
+        int sixDivTwo = six / two;
         int fiveMinusTwo = five - two;
         int fourTimeTwo = four * two;
         int onePlusTwo = one + two;
@@ -17,16 +17,3 @@ public class Calculator {
         System.out.println(fourTimeTwo);
     }
 }
-
-//1. В методе main создайте дополнительные переменные, кроме тех, что уже созданы.
-// - переменную типа int c именем six и значением 6.
-//        - переменную типа int c именем four и значением 4.
-//        - переменную типа int с именем five и значением 5.
-//
-//        2. Создайте еще три переменные для хранения результата арифметических операций.
-//  - переменную типа int c именем sixDivTwo и присвойте ей значение арифметической операции деления переменной six на two.
-//  - переменную типа int c именем fiveMinusTwo и присвойте ей значение арифметической операции вычитания переменной five c two.
-//  - переменную типа int c именем fourTimeTwo и присвойте ей значение арифметической операции умножения переменной four на two.
-//
-//3. Выведите на консоль переменные sixDivTwo, fiveMinusTwo, fourTimeTwo.
-
