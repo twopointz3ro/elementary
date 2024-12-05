b9fc3122deec63f55ac19030e4b5657fcb397842
twopointz3ro <twopointzero404@gmail.com>
2024-12-05 09:16:12 +0100

    исправил всё как было

--- src/main/java/ru/Main.java
+++ src/main/java/ru/Main.java
@@ -1,12 +1,7 @@
 package ru;
 
 public class Main {
-
-    public static int func1(int x) {
-        return x;
-    }
-
     public static void main(String[] args) {
-        int result3 = Main.func1(100);
+        System.out.println("Hello world");
     }
 }
