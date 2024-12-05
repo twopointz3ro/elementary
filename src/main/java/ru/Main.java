75146eddccb1845b3fd3e9f77945c7bab9a40237
twopointz3ro <twopointzero404@gmail.com>
2024-12-05 08:11:23 +0100

    1.7 Результат работы метода //результат не выводил на консоль, так как этого не было в задании, поэтому гит ругался, что result3 никогда не использовался.

--- src/main/java/ru/Main.java
+++ src/main/java/ru/Main.java
@@ -1,7 +1,12 @@
 package ru;
 
 public class Main {
+
+    public static int func1(int x) {
+        return x;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int result3 = Main.func1(100);
     }
 }
